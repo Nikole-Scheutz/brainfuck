6f8364f2dae7eb405814149bbcc67d31b32dcfbc
Nikole Scheutz <112065881+Nikole-Scheutz@users.noreply.github.com>
2023-12-04 22:08:07 +0100

    work on bf compiler, slow progress...

--- python/bf.py
+++ python/bf.py
@@ -1,38 +1,65 @@
 #!/usr/bin/env python3
 import ctypes
+from enum import Enum
+import re
 
-pointer = 0
-tape = [ctypes.c_byte() for _ in range(3000)]
 file = open("test.bf", "r").read()
+file = re.sub(r"[\n\t ]", "", file)
 
-def inc_dp():
-    global pointer
-    pointer += 1
+class Machine(object):
+    def __init__(self) -> None:
+        self.dp = 0
+        self.tape = [ctypes.c_byte() for _ in range(3000)]
+        self.dp_val = self.tape[0].value
+        pass
 
-def dec_dp():
-    global pointer
-    pointer -= 1
+    def inc_dp(self):
+        self.dp += 1
 
-def inc_val():
-    tape[pointer].value += 1
+    def dec_dp(self):
+        self.dp -= 1
 
-def dec_val():
-    tape[pointer].value -= 1
+    def inc_val(self):
+        self.tape[self.dp].value += 1
 
-def print_val():
-    print(tape[pointer].value)
+    def dec_val(self):
+        self.tape[self.dp].value -= 1
 
-for line in file:
-    for char in line:
-        match char:
-            case ">":
-                inc_dp()
-            case "<":
-                dec_dp()
-            case "+":
-                inc_val()
-            case "-":
-                dec_val()
-            case ".":
-                print_val()
+    def print_val(self):
+        print(self.tape[self.dp].value)
 
+    def parse(self, input):
+        commands = []
+        for char in input:
+            match char:
+                case ">":
+                    commands.append(self.inc_dp)
+                case "<":
+                    commands.append(self.dec_dp)
+                case "+":
+                    commands.append(self.inc_val)
+                case "-":
+                    commands.append(self.dec_val)
+                case ".":
+                    commands.append(self.print_val)
+        self.commands = commands
+
+    def run(self):
+        for command in self.commands:
+            command()
+
+    class Char(Enum):
+        Right = ">"
+        Left = "<"
+        Inc = "+"
+        Dec = "-"
+        Print = "."
+        Input = ","
+        StartLoop = "["
+        EndLoop = "]"
+
+
+
+machine = Machine()
+machine.parse(file)
+machine.run()
